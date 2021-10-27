2f2de06a66d2f29d9d4d1aef6c4543c1c250debe
saurabhchirde <saurabh123.chirde@gmail.com>
2021-10-27 18:34:37 +0530

    input out and button style

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-"use strict";
+// 'use strict';
 
 const txt = document.querySelector("#txt-input");
+const btntrans = document.querySelector("#btn-translate");
+const divOut = document.querySelector("#output");
+const btnclear = document.querySelector("#btn-clear");
+
+btntrans.addEventListener("click", function () {
+  divOut.innerText = txt.value;
+});
+
+btnclear.addEventListener("click", function () {
+  divOut.innerText = "";
+  txt.value = "";
+});
