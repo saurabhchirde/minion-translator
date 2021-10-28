f7c9d2d3f128c1f888ab6910f9c1080295bdfb2e
saurabhchirde <saurabh123.chirde@gmail.com>
2021-10-28 13:07:44 +0530

    connected to url

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const btntrans = document.querySelector("#btn-translate");
 const divOut = document.querySelector("#output");
 const btnclear = document.querySelector("#btn-clear");
 
+const serverURL = "https://api.saurabhchirde.repl.co/translate/yoda.json";
+
+function TranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
 btntrans.addEventListener("click", function () {
-  divOut.innerText = txt.value;
+  const txtInput = txt.value;
+
+  fetch(TranslationURL(txtInput))
+    .then(response => response.json())
+    .then(jsontxt => console.log(jsontxt.contents.translated));
+
+  // divOut.innerText = txt.value;
 });
 
 btnclear.addEventListener("click", function () {
